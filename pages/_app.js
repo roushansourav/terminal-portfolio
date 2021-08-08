d9c87540e1245cfc71dcf13b6d212c45acab8cf9
Roushan <sugoi-roushansourav@github.com>
2021-08-08 10:09:16 +0530

    fixes-viewport

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,15 @@
-import '/styles/index.scss';
-import Head from 'next/head'
+import "/styles/index.scss";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <meta name="viewport" content="viewport-fit=cover" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <Component {...pageProps} />
     </>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
